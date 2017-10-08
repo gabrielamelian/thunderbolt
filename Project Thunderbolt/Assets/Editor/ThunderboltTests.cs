a64c3c26a3e7722731faccda14cfeb9d5ee8d989
Pedro Worcel <pedro@worcel.com>
2017-10-08 23:08:42 +1300

    Create boilerplate for unit tests.

---
+++ Project Thunderbolt/Assets/Editor/ThunderboltTests.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ThunderboltTests {
+	[Test]
+	public void NewEditModeTestSimplePasses() {
+		// Use the Assert class to test conditions.
+		Assert.IsTrue(true);
+	}
+
+}
