4d1b6342c180570f67d50d8b54b7848fb02e7afb
Pedro Worcel <pedro@worcel.com>
2017-10-10 18:20:33 +1300

    Add ability to change levels.

---
+++ Project Thunderbolt/Assets/Scripts/ChangeToNextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeToNextLevel : MonoBehaviour {
+	public string nextLevel;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			SceneManager.LoadScene(this.nextLevel);
+		}
+	}
+}
+
