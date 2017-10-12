8c1106f7ffd7d64e333ff85fcbbf741ef2b01e85
Pedro Worcel <pedro@worcel.com>
2017-10-13 07:01:06 +1300

    Interim movement commit.

---
+++ Project Thunderbolt/Assets/Scripts/Level.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Thunderbolt { 
+
+    public interface ILevel {
+
+    }
+
+    public enum Direction {
+        Left,
+        Right
+    }
+
+
+    public class Level : ILevel {
+
+        public Collider2D GetNextBlock(Transform player, Direction dir) {
+            Transform groundCheck = player.Find("GroundCheck");
+            LayerMask layer = LayerMask.NameToLayer("Block");
+
+            Collider2D blockBelow = Physics2D.OverlapCircle(groundCheck.position, .2f, 1 << layer);
+            Vector2 positionBelow = blockBelow.transform.position;
+
+            float xDelta = dir == Direction.Left ? -1 : 1;
+            Vector2 positionNext = positionBelow + new Vector2(xDelta, 0f);
+            Collider2D nextBlock = Physics2D.OverlapPoint(positionNext, 1 << layer);
+
+            return nextBlock;
+        }
+
+        public Vector2 GetTargetPosition(Transform player, Direction dir) {
+            Collider2D nextBlock = GetNextBlock(player, dir);
+            Vector2 targetPos;
+            if(nextBlock != null) {
+                float middleDelta = dir == Direction.Left ? -0.5f : 0.5f;
+                Vector2 center = (Vector2) nextBlock.transform.position + new Vector2(middleDelta, 0f);
+                targetPos = new Vector2(center.x, player.transform.position.y);
+            } else {
+                float xDelta = dir == Direction.Left ? -1f : 1f;
+                targetPos = (Vector2) player.transform.position + new Vector2(xDelta, 0f);
+            }
+
+            return targetPos;
+        }
+
+    }
+
+}
--- Project Thunderbolt/Assets/Scripts/PlatformerCharacter2D.cs
+++ Project Thunderbolt/Assets/Scripts/PlatformerCharacter2D.cs
@@ -1,40 +1,42 @@
 using System;
 using UnityEngine;
 
-namespace UnityStandardAssets._2D
-{
-    public class PlatformerCharacter2D : MonoBehaviour
-    {
-        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-        [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
-        [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
-        [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+namespace Thunderbolt { 
 
-        private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-        const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool m_Grounded;            // Whether or not the player is grounded.
-        private Transform m_CeilingCheck;   // A position marking where to check for ceilings
-        const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-        private Animator m_Anim;            // Reference to the player's animator component.
+    public class PlatformerCharacter2D : MonoBehaviour {
+
+        [SerializeField] private float m_MaxSpeed = 10f;                    
+        [SerializeField] private float m_JumpForce = 400f;                  
+        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f; 
+        [SerializeField] private bool m_AirControl = false;                 
+        [SerializeField] private LayerMask m_WhatIsGround;                  
+
+        private Transform m_GroundCheck;    
+        const float k_GroundedRadius = .2f; 
+        private bool m_Grounded;            
+        private Transform m_CeilingCheck;   
+        const float k_CeilingRadius = .01f;
+        private Animator m_Anim;            
         private Rigidbody2D m_Rigidbody2D;
-        private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+        private bool m_FacingRight = true;  
+
+        private bool stepping = false;
+        private Vector2 targetPosition;
+
+        private Level level = new Level();
 
         private void Awake()
         {
-            // Setting up references.
             m_GroundCheck = transform.Find("GroundCheck");
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-		private void FixedUpdate()
-        {
+        private void FixedUpdate() {
+
             m_Grounded = false;
 
-            // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-            // This can be done using layers instead but Sample Assets will not overwrite your project settings.
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
@@ -43,65 +45,85 @@ namespace UnityStandardAssets._2D
             }
             m_Anim.SetBool("Ground", m_Grounded);
 
-            // Set the vertical animation
             m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
         }
 
-		/// <summary>
-		/// Move the player according to the mechanics established by the game.
-		/// </summary>
-		/// <param name="move">Move. This number corresponds to a float between 0 (not moving) and 1 (moving at full speed)</param>
-		/// <param name="crouch">If set to <c>true</c> crouch.</param>
-		/// <param name="jump">If set to <c>true</c> jump.</param>
+        /// <summary>
+        /// Move the player according to the mechanics established by the game. Movement occurs in two stages. 
+        /// 
+        /// The first stage is triggered by any press of the left or right buttons and causes the character to move an entire "block". If subsequently the player continues to hold either the left or right buttons motion continues until the button is released. At that point the character continues until the next "block" and then stops.
+        /// </summary>
+        /// <param name="move">Move. This number corresponds to a float between 0 (not moving) and 1 (moving at full speed)</param>
+        /// <param name="crouch">If set to <c>true</c> crouch.</param>
+        /// <param name="jump">If set to <c>true</c> jump.</param>
         public void Move(float move, bool crouch, bool jump) {
-            // If crouching, check to see if the character can stand up
-            if (!crouch && m_Anim.GetBool("Crouch"))
-            {
-                // If the character has a ceiling preventing them from standing up, keep them crouching
-                if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-                {
-                    crouch = true;
-                }
+            float moveAbs = Math.Abs (move);
+            bool initiateStep = moveAbs > 0 && moveAbs < 1;
+
+            if (!stepping && initiateStep) {				
+                Direction direction = move < 0 ? Direction.Left : Direction.Right;
+                targetPosition = level.GetTargetPosition(this.transform, direction);
+                stepping = true;
             }
 
-            // Set whether or not the character is crouching in the animator
-            m_Anim.SetBool("Crouch", crouch);
-
-            //only control the player if grounded or airControl is turned on
-            if (m_Grounded || m_AirControl)
-            {
-                // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
-
-                // The Speed animator parameter is set to the absolute value of the horizontal input.
-                m_Anim.SetFloat("Speed", Mathf.Abs(move));
-
-                // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
-
-                // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
-                    // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
+            if(stepping == true) {
+                m_Rigidbody2D.position = Vector2.Lerp(m_Rigidbody2D.position, targetPosition, Time.fixedDeltaTime);
+                m_Anim.SetFloat("Speed", 4f);
             }
-            // If the player should jump...
-            if (m_Grounded && jump && m_Anim.GetBool("Ground"))
-            {
-                // Add a vertical force to the player.
-                m_Grounded = false;
-                m_Anim.SetBool("Ground", false);
-                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-            }
+
+            //			if (!
+
+
+
+            //        public void OldMove(float move, bool crouch, bool jump) {
+            //            // If crouching, check to see if the character can stand up
+            //            if (!crouch && m_Anim.GetBool("Crouch"))
+            //            {
+            //                // If the character has a ceiling preventing them from standing up, keep them crouching
+            //                if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            //                {
+            //                    crouch = true;
+            //                }
+            //            }
+            //
+            //            // Set whether or not the character is crouching in the animator
+            //            m_Anim.SetBool("Crouch", crouch);
+            //
+            //            //only control the player if grounded or airControl is turned on
+            //            if (m_Grounded || m_AirControl)
+            //            {
+            //                // Reduce the speed if crouching by the crouchSpeed multiplier
+            //                move = (crouch ? move*m_CrouchSpeed : move);
+            //
+            //                // The Speed animator parameter is set to the absolute value of the horizontal input.
+            //                m_Anim.SetFloat("Speed", Mathf.Abs(move));
+            //
+            //                // Move the character
+            //                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+            //
+            //                // If the input is moving the player right and the player is facing left...
+            //                if (move > 0 && !m_FacingRight)
+            //                {
+            //                    // ... flip the player.
+            //                    Flip();
+            //                }
+            //                    // Otherwise if the input is moving the player left and the player is facing right...
+            //                else if (move < 0 && m_FacingRight)
+            //                {
+            //                    // ... flip the player.
+            //                    Flip();
+            //                }
+            //            }
+            //            // If the player should jump...
+            //            if (m_Grounded && jump && m_Anim.GetBool("Ground"))
+            //            {
+            //                // Add a vertical force to the player.
+            //                m_Grounded = false;
+            //                m_Anim.SetBool("Ground", false);
+            //                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            //            }
         }
-
+        //
 
         private void Flip()
         {
@@ -113,5 +135,5 @@ namespace UnityStandardAssets._2D
             theScale.x *= -1;
             transform.localScale = theScale;
         }
+        }
     }
-}
